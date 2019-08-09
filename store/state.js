b0143b80327706a00a998c9139b6c58626ad8326
Nurfirliana Muzanella <muzanella11@gmail.com>
2019-08-10 00:48:05 +0800

    Initialize

---
+++ store/state.js
@@ -0,0 +1,20 @@
+export default {
+  layoutSettings: {
+    titleApps: null,
+    drawer: null,
+    toolbar: true,
+    breadcrumbVisibility: true,
+    breadcrumbs: []
+  },
+  isOnline: true,
+  isLoading: {
+    example: false
+  },
+  isTimeout: {
+    example: false
+  },
+  messagesConnection: {
+    offline: 'Anda berada pada mode offline',
+    online: 'Anda telah terhubung kembali'
+  }
+}
