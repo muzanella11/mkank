b33df537497219a4fd0bc50e5a3addd6bf7febb6
YogaDarma182 <yogadarma182@gmail.com>
2019-08-15 23:22:41 +0800

    [UPDATE] Add listing page

---
+++ pages/listing/register/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app id="seller-registration">
+    <v-content
+      ref="mainContent"
+      class="main blank"
+    >
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<style lang="scss" scoped src="./style.scss"></style>
+
+<script lang="js" src="./script.js"></script>
